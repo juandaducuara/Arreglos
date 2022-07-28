7e08c598ce3473bde8058489fec06fad0612395a
juandaducuara <jdducuara63@misena.edu.co>
2022-07-27 23:52:02 -0500

    Agregar archivos de proyecto.

---
+++ Arreglos/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            metodos metodo = new metodos();
+            string punto = "";
+            do
+            {
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("<=======================================================>");
+            Console.WriteLine("Primer actividad trimestre");
+            Console.WriteLine("Digite el punto deseado");
+            punto = Console.ReadLine();
+                switch (punto)
+                {
+                    case "1": primerPunto();
+                        break;
+                    case "2": segundoPunto();
+                        break;
+                    case "3": tercerPunto();
+                        break;
+                    case "4":
+                        metodo.PrimerMetodo();
+                        break;
+                    case "5":
+                        metodo.segundoMetodo();
+                        break;
+                    case "6":
+                        metodo.tercerMetodo();
+                        break;
+                    case "7":
+                        metodo.cuartoMetodo();
+                        break;
+                    case "0":
+                        Console.WriteLine("Saliendo del programa");
+                        break;
+
+                    default:
+                            Console.WriteLine("opcion no valida");
+                        break;
+            }
+            } while (punto!="0");
+
+        }
+        static void primerPunto()
+        {
+            int[] cantidadalum = new int[4];
+            string[] nombrealum = new string[4];
+            int[] examenp = new int[4];
+            int[] exameno = new int[4];
+            int[] examenf = new int[4];
+            int[] promedio = new int[4];
+            for (int c = 0; c < cantidadalum.Length; c++)
+            {
+                Console.WriteLine("Introdusca el nombre del alumno");
+                Console.Write("Nombre alumno ==> ");
+                nombrealum[c] = Console.ReadLine();
+                Console.WriteLine($"Introduzca las notas de {nombrealum[c]}");
+                Console.Write("Examen Parcial ==> ");
+                examenp[c] = int.Parse(Console.ReadLine());
+                Console.Write("Examen Oral ==> ");
+                exameno[c] = int.Parse(Console.ReadLine());
+                Console.Write("Examen Final ==> ");
+                examenf[c] = int.Parse(Console.ReadLine());
+                Console.WriteLine(" ");
+                for (int i = 0; i < promedio.Length; i++)
+                {
+                    promedio[i] = (examenp[i] + exameno[i] + examenf[i]) / 3;
+                }
+
+            }
+
+            for (int i = 0; i < cantidadalum.Length; i++)
+            {
+                Console.WriteLine($"Nombre :{nombrealum[i]}\n Examen parcial: {examenp[i]} \n Examen oral : {exameno[i]} Examen final: {examenf[i]} Promedio: {promedio[i]}");
+                if (promedio[i] >= 7)
+                {
+                    Console.WriteLine("APROBADO");
+                }
+                else
+                {
+                    Console.WriteLine("NO APROBADO");
+                }
+            }
+        }
+        static void segundoPunto()
+        {
+            string[] nombre = new string[5];
+            string[] apellido = new string[5];
+            int[] edad = new int[5];
+            string[]sexo = new string[5];
+
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                Console.WriteLine($"Introduzca el nombre del empleado {i+1}");
+                nombre[i] = Console.ReadLine();
+                Console.WriteLine($"Introduzca el apellido del empleado ");
+                apellido[i] = Console.ReadLine();
+                Console.WriteLine($"Introduzca la edad del empleado");
+                edad[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Introduzca el sexo del empleado");
+                sexo[i] = Console.ReadLine();
+                Console.WriteLine("<=======================================================>");
+            }
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                Console.WriteLine($"Datos empleado {i+1} \n Nombre : {nombre[i]} \n Apellidos :{apellido[i]} \n Edad : {edad[i]} \n Sexo: {sexo[i]} ");
+                Console.WriteLine("<=======================================================>");
+            }
+        }
+
+        static void tercerPunto()
+        {
+            int totalPago=0;
+            Console.WriteLine("Digite la cantidad de productos que quiere ingresar ");
+            int cantidadProd = int.Parse(Console.ReadLine());
+            int [] precios = new int[cantidadProd];
+
+            for (int i = 0; i < precios.Length; i++)
+            {
+                Console.Write("Digite el precio del producto " + (i+1) + "\n$");                
+                precios[i] = Convert.ToInt32(Console.ReadLine());
+                totalPago = precios[i] + totalPago;
+                Console.WriteLine("<=======================================================>");
+            }
+            Console.WriteLine("Valor total a pagar = $" + totalPago); 
+        }
+
+    }
+}
---
+++ Arreglos/metodos.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    class metodos
+    {
+        public void PrimerMetodo()
+        {
+            double dolar = 4429;
+            double reales = 844;
+            double rublo = 73.92;
+            double euro = 4522;
+            double yen = 32.73;
+            Console.WriteLine(" Digite la moneda que desea convertir \n 1. Dolar \n 2.Reales \n 3.Rublo \n 4.Euro \n 5.Yen");
+            string opcion = Console.ReadLine();
+            Console.WriteLine("Digite el cantidad de dinero");
+            double cantidadDin = double.Parse(Console.ReadLine());
+            double conversion = 0;
+            switch (opcion)
+            {
+                case "1" :
+                    conversion = cantidadDin * dolar;
+                    Console.WriteLine($"Cantidad Dinero en US$ {cantidadDin}");
+                    Console.WriteLine($"Cantidad Dinero en COP$ {conversion}");
+                    break;
+                case "2":
+                    conversion = cantidadDin * reales;
+                    Console.WriteLine($"Cantidad Dinero en R$ {cantidadDin}");
+                    Console.WriteLine($"Cantidad Dinero en COP$ {conversion}");
+                    break;
+                case "3":
+                    conversion = cantidadDin * rublo;
+                    Console.WriteLine($"Cantidad Dinero en ₽ {cantidadDin}");
+                    Console.WriteLine($"Cantidad Dinero en COP$ {conversion}");
+                    break;
+                case "4":
+                    conversion = cantidadDin * euro;
+                    Console.WriteLine($"Cantidad Dinero en € {cantidadDin}");
+                    Console.WriteLine($"Cantidad Dinero en COP$ {conversion}");
+                    break;
+                case "5":
+                    conversion = cantidadDin * yen;
+                    Console.WriteLine($"Cantidad Dinero en ¥ {cantidadDin}");
+                    Console.WriteLine($"Cantidad Dinero en COP$ {conversion}");
+                    break;                
+                default:
+                    Console.WriteLine("Opcion invalida");
+                    break;
+            }
+        }
+
+        public void segundoMetodo()
+        {
+            double lado_1 = 0;
+            double lado_2 = 0;
+            Console.WriteLine("Ingrese el valor del primer lado del rectángulo:");
+            lado_1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del primer lado del rectángulo:");
+            lado_2 = double.Parse(Console.ReadLine());
+            double area = lado_1 * lado_2;
+            Console.WriteLine("El área de su rectangulo es de: " + area);
+        }
+        public void tercerMetodo()
+        {
+            try
+            {
+                int edad = 0;
+                Console.WriteLine("Digite su edad ");
+                edad = Convert.ToInt32(Console.ReadLine());
+
+                if (edad < 18)
+                {
+                    Console.WriteLine("Usted es menor de edad");
+                }
+                else if (edad < 60)
+                {
+                    Console.WriteLine("Usted es mayor de edad");
+                }
+                else
+                {
+                    Console.WriteLine("Usted es persona adulto mayor");
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error caracter no valido");
+            }
+
+        }
+        public void cuartoMetodo()
+        {
+            int numero = 0;
+            Console.WriteLine("Digite la tabla de multiplicar que desea");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 13; i++)
+            {
+                Console.WriteLine($"{numero} * {i} = {numero * i}");
+            }
+
+        }
+    }
+}
